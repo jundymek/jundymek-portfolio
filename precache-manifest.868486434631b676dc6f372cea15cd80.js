2e9a641511041ef2c9751c94569e80133b8f58d5
jundymek <jundymek@gmail.com>
2020-03-31 19:57:08 +0200

    Updates

---
+++ precache-manifest.868486434631b676dc6f372cea15cd80.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c75236ec224dba6e40f5bcfdcb457df",
+    "url": "/jundymek-portfolio/index.html"
+  },
+  {
+    "revision": "cbb39c186924a2303862",
+    "url": "/jundymek-portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e6d219fbc16c6ec538d2",
+    "url": "/jundymek-portfolio/static/js/2.fe57acc0.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/jundymek-portfolio/static/js/2.fe57acc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbb39c186924a2303862",
+    "url": "/jundymek-portfolio/static/js/main.5e66726b.chunk.js"
+  },
+  {
+    "revision": "b3b74a7c4df09f9c254f",
+    "url": "/jundymek-portfolio/static/js/runtime-main.5ca070d0.js"
+  },
+  {
+    "revision": "7dcc03cf8aafb195d8bba7d22bb87738",
+    "url": "/jundymek-portfolio/static/media/codewars-icon.7dcc03cf.svg"
+  },
+  {
+    "revision": "df2a153be11d45cef48915c68c951732",
+    "url": "/jundymek-portfolio/static/media/css3-icon.df2a153b.svg"
+  },
+  {
+    "revision": "3ea7a7e767a71a66934db27406487157",
+    "url": "/jundymek-portfolio/static/media/django-icon.3ea7a7e7.svg"
+  },
+  {
+    "revision": "5f44b16eff13f1742bc31724b5cc8fd3",
+    "url": "/jundymek-portfolio/static/media/flip-icon.5f44b16e.svg"
+  },
+  {
+    "revision": "8c6e70baad4088ab4eb2eeee665b935f",
+    "url": "/jundymek-portfolio/static/media/free_proxy.8c6e70ba.gif"
+  },
+  {
+    "revision": "6f4b432cc4943ebab8ad595915222759",
+    "url": "/jundymek-portfolio/static/media/git-icon.6f4b432c.svg"
+  },
+  {
+    "revision": "4097a5be85bf4c4e64ec52f389ea039d",
+    "url": "/jundymek-portfolio/static/media/github-icon.4097a5be.svg"
+  },
+  {
+    "revision": "ba679906e271d70b0c6c0f2460e8d2e5",
+    "url": "/jundymek-portfolio/static/media/github-icon.ba679906.svg"
+  },
+  {
+    "revision": "082fbf303d3d4f9ebeec67b5211d8b56",
+    "url": "/jundymek-portfolio/static/media/hero-image-big-min.082fbf30.png"
+  },
+  {
+    "revision": "ae7d81b389b5d3137bfcfb64318cad6d",
+    "url": "/jundymek-portfolio/static/media/hero-image-small-min.ae7d81b3.png"
+  },
+  {
+    "revision": "67d978951e48c3e185e053b9fefa8afb",
+    "url": "/jundymek-portfolio/static/media/html5-icon.67d97895.svg"
+  },
+  {
+    "revision": "de79c8f14c2b0d692ae215d7e6eccd54",
+    "url": "/jundymek-portfolio/static/media/javascript-icon.de79c8f1.svg"
+  },
+  {
+    "revision": "d3ac607a9624aa7379d54013135dadd9",
+    "url": "/jundymek-portfolio/static/media/linkedin-icon.d3ac607a.svg"
+  },
+  {
+    "revision": "53e15d1af084e2cf125d5b3dd2cb409a",
+    "url": "/jundymek-portfolio/static/media/netkat-project.53e15d1a.png"
+  },
+  {
+    "revision": "3f0dd61423cb3026e5543a3922baf754",
+    "url": "/jundymek-portfolio/static/media/other-icon.3f0dd614.svg"
+  },
+  {
+    "revision": "776c2668866bb6c03b3c6fbeacc2d7b2",
+    "url": "/jundymek-portfolio/static/media/pan-kanapka-project.776c2668.png"
+  },
+  {
+    "revision": "9474528ec861c5d35b9efd498ddef326",
+    "url": "/jundymek-portfolio/static/media/postgresql-icon.9474528e.svg"
+  },
+  {
+    "revision": "3b84eb4026f0c704e7e3d16972bb0e3d",
+    "url": "/jundymek-portfolio/static/media/python-icon.3b84eb40.svg"
+  },
+  {
+    "revision": "fadd9c9566ccc46c6b0e73caca5860de",
+    "url": "/jundymek-portfolio/static/media/react-icon.fadd9c95.svg"
+  },
+  {
+    "revision": "88c1047792044b547d1d3e066d3e18c4",
+    "url": "/jundymek-portfolio/static/media/sass-icon.88c10477.svg"
+  },
+  {
+    "revision": "0ecd0125f31e0bfc39a9d3e130ca6cd6",
+    "url": "/jundymek-portfolio/static/media/typescript-icon.0ecd0125.svg"
+  }
+]);
