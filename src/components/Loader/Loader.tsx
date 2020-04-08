3776cd719500f70b16c2be836795b3925fa1a0d1
jundymek <jundymek@gmail.com>
2020-04-08 18:24:15 +0200

    refactor: BackgroundMixin

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
-import { TerminalLookMixin } from "../../styles/styledComponents";
+import { TerminalLookMixin, BackgroundMixin } from "../../styles/styledComponents";
 
 export const scaling = keyframes`
   0%   {transform: scaleY(1);}
@@ -12,52 +12,7 @@ const Wrapper = styled.div`
   justify-content: center;
   background: black;
   width: 90%;
-  box-shadow: 6px 4px 4px -1px rgba(0, 0, 0, 0.38);
-  background: linear-gradient(
-      216deg,
-      rgba(77, 77, 77, 0.05) 0%,
-      rgba(77, 77, 77, 0.05) 25%,
-      rgba(42, 42, 42, 0.05) 25%,
-      rgba(42, 42, 42, 0.05) 38%,
-      rgba(223, 223, 223, 0.05) 38%,
-      rgba(223, 223, 223, 0.05) 75%,
-      rgba(36, 36, 36, 0.05) 75%,
-      rgba(36, 36, 36, 0.05) 100%
-    ),
-    linear-gradient(
-      44deg,
-      rgba(128, 128, 128, 0.05) 0%,
-      rgba(128, 128, 128, 0.05) 34%,
-      rgba(212, 212, 212, 0.05) 34%,
-      rgba(212, 212, 212, 0.05) 57%,
-      rgba(25, 25, 25, 0.05) 57%,
-      rgba(25, 25, 25, 0.05) 89%,
-      rgba(135, 135, 135, 0.05) 89%,
-      rgba(135, 135, 135, 0.05) 100%
-    ),
-    linear-gradient(
-      241deg,
-      rgba(55, 55, 55, 0.05) 0%,
-      rgba(55, 55, 55, 0.05) 14%,
-      rgba(209, 209, 209, 0.05) 14%,
-      rgba(209, 209, 209, 0.05) 60%,
-      rgba(245, 245, 245, 0.05) 60%,
-      rgba(245, 245, 245, 0.05) 69%,
-      rgba(164, 164, 164, 0.05) 69%,
-      rgba(164, 164, 164, 0.05) 100%
-    ),
-    linear-gradient(
-      249deg,
-      rgba(248, 248, 248, 0.05) 0%,
-      rgba(248, 248, 248, 0.05) 32%,
-      rgba(148, 148, 148, 0.05) 32%,
-      rgba(148, 148, 148, 0.05) 35%,
-      rgba(202, 202, 202, 0.05) 35%,
-      rgba(202, 202, 202, 0.05) 51%,
-      rgba(181, 181, 181, 0.05) 51%,
-      rgba(181, 181, 181, 0.05) 100%
-    ),
-    linear-gradient(92deg, hsl(214, 0%, 11%), hsl(214, 0%, 11%));
+  ${BackgroundMixin};
   @media (min-width: ${props => props.theme.desktop}) {
     width: 100%;
   }
--- src/components/Repositories/Repositories.tsx
+++ src/components/Repositories/Repositories.tsx
@@ -11,7 +11,7 @@ import typescriptImage from "../../images/skills-icons/typescript-icon.svg";
 import htmlImage from "../../images/skills-icons/html5-icon.svg";
 import otherImage from "../../images/skills-icons/other-icon.svg";
 import useOnScreen from "../../customHooks/useOnScreen";
-import { SectionSubtitle } from "../../styles/styledComponents";
+import { SectionSubtitle, BackgroundMixin } from "../../styles/styledComponents";
 import RepositoriesCounter from "./RepositioriesCounter";
 import Loader from "../Loader/Loader";
 
@@ -88,52 +88,7 @@ const Technology = styled.span<Width>`
   text-align: center;
   padding: 6px;
   border-radius: 2px;
-  box-shadow: 6px 4px 4px -1px rgba(0, 0, 0, 0.38);
-  background: linear-gradient(
-      216deg,
-      rgba(77, 77, 77, 0.05) 0%,
-      rgba(77, 77, 77, 0.05) 25%,
-      rgba(42, 42, 42, 0.05) 25%,
-      rgba(42, 42, 42, 0.05) 38%,
-      rgba(223, 223, 223, 0.05) 38%,
-      rgba(223, 223, 223, 0.05) 75%,
-      rgba(36, 36, 36, 0.05) 75%,
-      rgba(36, 36, 36, 0.05) 100%
-    ),
-    linear-gradient(
-      44deg,
-      rgba(128, 128, 128, 0.05) 0%,
-      rgba(128, 128, 128, 0.05) 34%,
-      rgba(212, 212, 212, 0.05) 34%,
-      rgba(212, 212, 212, 0.05) 57%,
-      rgba(25, 25, 25, 0.05) 57%,
-      rgba(25, 25, 25, 0.05) 89%,
-      rgba(135, 135, 135, 0.05) 89%,
-      rgba(135, 135, 135, 0.05) 100%
-    ),
-    linear-gradient(
-      241deg,
-      rgba(55, 55, 55, 0.05) 0%,
-      rgba(55, 55, 55, 0.05) 14%,
-      rgba(209, 209, 209, 0.05) 14%,
-      rgba(209, 209, 209, 0.05) 60%,
-      rgba(245, 245, 245, 0.05) 60%,
-      rgba(245, 245, 245, 0.05) 69%,
-      rgba(164, 164, 164, 0.05) 69%,
-      rgba(164, 164, 164, 0.05) 100%
-    ),
-    linear-gradient(
-      249deg,
-      rgba(248, 248, 248, 0.05) 0%,
-      rgba(248, 248, 248, 0.05) 32%,
-      rgba(148, 148, 148, 0.05) 32%,
-      rgba(148, 148, 148, 0.05) 35%,
-      rgba(202, 202, 202, 0.05) 35%,
-      rgba(202, 202, 202, 0.05) 51%,
-      rgba(181, 181, 181, 0.05) 51%,
-      rgba(181, 181, 181, 0.05) 100%
-    ),
-    linear-gradient(92deg, hsl(214, 0%, 11%), hsl(214, 0%, 11%));
+  ${BackgroundMixin};
   @media (min-width: ${props => props.theme.desktop}) {
     padding: 10px;
   }
