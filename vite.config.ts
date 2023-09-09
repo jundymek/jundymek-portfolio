02c413e31cffa2391c1a95bb22716c78c2ea78f8
jundymek <jundymek@gmail.com>
2023-09-09 14:30:38 +0200

    chore: change coniguration files for gh-pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,13 @@
-import { defineConfig, Plugin } from "vite"
-import { resolve } from "path"
+import { defineConfig } from "vite"
+
 import react from "@vitejs/plugin-react"
 import viteTsconfigPaths from "vite-tsconfig-paths"
 import svgrPlugin from "vite-plugin-svgr"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    // checker({
-    //   overlay: { initialIsOpen: false },
-    //   typescript: true,
-    //   eslint: {
-    //     lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
-    //   },
-    // }),
-    viteTsconfigPaths(),
-    svgrPlugin(),
-    // handlebars({
-    //   partialDirectory: resolve(__dirname, "src/partials"),
-    // }) as Plugin,
-  ],
+  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  base: "/",
   server: {
     port: 3000,
     proxy: {
